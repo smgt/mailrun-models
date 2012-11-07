76471691c0fdd22b4765e67750c5a54208336979
Simon Gate <simon@smgt.me>
2012-11-07 11:39:48 +0100

    Add virtual attribute body on template

--- template.rb
+++ template.rb
@@ -1,6 +1,6 @@
 class Template < ActiveRecord::Base
 
-  attr_accessible :body_html, :body_text, :generate_text, :name, :subject
+  attr_accessible :body_html, :body_text, :generate_text, :name, :subject, :body
   attr_readonly :user_id, :ident
 
   belongs_to :user
@@ -9,11 +9,23 @@ class Template < ActiveRecord::Base
 
   before_create :assign_ident
 
+  def body=(body={})
+    self.body_html = body[:html] if body[:html]
+    self.body_text = body[:text] if body[:text]
+  end
+
+  def body
+    {
+      html: body_html,
+      text: body_text
+    }
+  end
+
   private
 
   def assign_ident
     ident = name.downcase.gsub(%r([^a-z\d\s]), "").strip.gsub(%r(\s), "-")
-    self.ident = ident.blank? ? created_at.to_i.to_s : ident
+    self.ident = ident.blank? ? Time.now.to_i.to_s : ident
   end
 
 end
